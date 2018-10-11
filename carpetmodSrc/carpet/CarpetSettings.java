c405e6502bdc3898451cecd15489e17d178e89d5
Earthcomputer <burtonjae@hotmail.co.uk>
2018-10-11 11:19:32 +0100

    Fix missing import

--- carpetmodSrc/carpet/CarpetSettings.java
+++ carpetmodSrc/carpet/CarpetSettings.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
+import carpet.carpetclient.CarpetClientChunkLogger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
