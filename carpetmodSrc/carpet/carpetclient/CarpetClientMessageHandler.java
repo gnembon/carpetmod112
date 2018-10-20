bc3f1c618e7d83bcb5f9bbda48f19cf6bd684880
Unknown <sahandhesar@gmail.com>
2018-10-20 05:33:53 +0200

    Cleanup and try catch for crash prevention

--- carpetmodSrc/carpet/carpetclient/CarpetClientMessageHandler.java
+++ carpetmodSrc/carpet/carpetclient/CarpetClientMessageHandler.java
@@ -13,151 +13,152 @@ import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.PacketBuffer;
 
 public class CarpetClientMessageHandler {
-	// Main packet data names
-	public static final int GUI_ALL_DATA = 0;
-	public static final int RULE_REQUEST = 1;
-	public static final int VILLAGE_MARKERS = 2;
-	public static final int BOUNDINGBOX_MARKERS = 3;
-	public static final int TICKRATE_CHANGES = 4;
-	public static final int LARGE_VILLAGE_MARKERS_START = 5;
-	public static final int LARGE_VILLAGE_MARKERS = 6;
-	public static final int LARGE_BOUNDINGBOX_MARKERS_START = 7;
-	public static final int LARGE_BOUNDINGBOX_MARKERS = 8;
-	public static final int CHUNK_LOGGER = 9;
-	
-	public static void handler(EntityPlayerMP sender, PacketBuffer data) {
-		int type = data.readInt();
+    // Main packet data names
+    public static final int GUI_ALL_DATA = 0;
+    public static final int RULE_REQUEST = 1;
+    public static final int VILLAGE_MARKERS = 2;
+    public static final int BOUNDINGBOX_MARKERS = 3;
+    public static final int TICKRATE_CHANGES = 4;
+    public static final int LARGE_VILLAGE_MARKERS_START = 5;
+    public static final int LARGE_VILLAGE_MARKERS = 6;
+    public static final int LARGE_BOUNDINGBOX_MARKERS_START = 7;
+    public static final int LARGE_BOUNDINGBOX_MARKERS = 8;
+    public static final int CHUNK_LOGGER = 9;
 
-		if (GUI_ALL_DATA == type) {
-			sendAllGUIOptions();
-		} else if (RULE_REQUEST == type) {
-			ruleRequest(sender, data);
-		} else if (VILLAGE_MARKERS == type) {
-			registerVillagerMarkers(sender, data);
-		} else if (BOUNDINGBOX_MARKERS == type) {
-			boundingboxRequest(sender, data);
-		} else if (CHUNK_LOGGER == type) {
-			CarpetClientChunkLogger.logger.registerPlayer(sender, data);
-		}
-	}
+    public static void handler(EntityPlayerMP sender, PacketBuffer data) {
+        int type = data.readInt();
 
-	private static void registerVillagerMarkers(EntityPlayerMP sender, PacketBuffer data) {
-		CarpetClientMarkers.registerVillagerMarkers(sender, data);
-	}
+        if (GUI_ALL_DATA == type) {
+            sendAllGUIOptions();
+        } else if (RULE_REQUEST == type) {
+            ruleRequest(sender, data);
+        } else if (VILLAGE_MARKERS == type) {
+            registerVillagerMarkers(sender, data);
+        } else if (BOUNDINGBOX_MARKERS == type) {
+            boundingboxRequest(sender, data);
+        } else if (CHUNK_LOGGER == type) {
+            CarpetClientChunkLogger.logger.registerPlayer(sender, data);
+        }
+    }
 
-	private static void boundingboxRequest(EntityPlayerMP sender, PacketBuffer data) {
-		CarpetClientMarkers.updateClientBoundingBoxMarkers(sender, data);
-	}
+    private static void registerVillagerMarkers(EntityPlayerMP sender, PacketBuffer data) {
+        CarpetClientMarkers.registerVillagerMarkers(sender, data);
+    }
 
-	private static void ruleRequest(EntityPlayerMP sender, PacketBuffer data) {
-		CarpetClientRuleChanger.ruleChanger(sender, data);
-	}
+    private static void boundingboxRequest(EntityPlayerMP sender, PacketBuffer data) {
+        CarpetClientMarkers.updateClientBoundingBoxMarkers(sender, data);
+    }
 
-	public static void sendAllGUIOptions() {
-		PacketBuffer data = new PacketBuffer(Unpooled.buffer());
-		data.writeInt(GUI_ALL_DATA);
+    private static void ruleRequest(EntityPlayerMP sender, PacketBuffer data) {
+        CarpetClientRuleChanger.ruleChanger(sender, data);
+    }
 
-		ArrayList<CarpetSettingEntry> list = CarpetSettings.getAllCarpetSettings();
+    public static void sendAllGUIOptions() {
+        PacketBuffer data = new PacketBuffer(Unpooled.buffer());
+        data.writeInt(GUI_ALL_DATA);
 
-		data.writeString(CarpetSettings.carpetVersion);
-		data.writeFloat(TickSpeed.tickrate);
-		data.writeInt(list.size());
-		for (CarpetSettingEntry cse : list) {
-			String rule = cse.getName();
-			String current = cse.getStringValue();
-			String[] options = cse.getOptions();
-			String def = cse.getDefault();
-			boolean isfloat = cse.getIsFloat();
+        ArrayList<CarpetSettingEntry> list = CarpetSettings.getAllCarpetSettings();
 
-			data.writeString(rule);
-			data.writeString(current);
-			data.writeString(def);
-			data.writeBoolean(isfloat);
-			// data.writeInt(options.length);
-			// for (String o : options) {
-			// data.writeString(o);
-			// }
-		}
+        data.writeString(CarpetSettings.carpetVersion);
+        data.writeFloat(TickSpeed.tickrate);
+        data.writeInt(list.size());
+        for (CarpetSettingEntry cse : list) {
+            String rule = cse.getName();
+            String current = cse.getStringValue();
+            String[] options = cse.getOptions();
+            String def = cse.getDefault();
+            boolean isfloat = cse.getIsFloat();
 
-		CarpetClientServer.sender(data);
-	}
+            data.writeString(rule);
+            data.writeString(current);
+            data.writeString(def);
+            data.writeBoolean(isfloat);
+            // data.writeInt(options.length);
+            // for (String o : options) {
+            // data.writeString(o);
+            // }
+        }
 
-	public static void sendNBTVillageData(EntityPlayerMP sender, NBTTagCompound compound) {
-		PacketBuffer data = new PacketBuffer(Unpooled.buffer());
-		data.writeInt(CarpetClientMessageHandler.VILLAGE_MARKERS);
+        CarpetClientServer.sender(data);
+    }
 
-		data.writeCompoundTag(compound);
+    public static void sendNBTVillageData(EntityPlayerMP sender, NBTTagCompound compound) {
+        PacketBuffer data = new PacketBuffer(Unpooled.buffer());
+        data.writeInt(CarpetClientMessageHandler.VILLAGE_MARKERS);
 
-		if (!CarpetClientServer.sendProtected(data, sender)) {
-		    // Payload was too large, try large packets for newer CC versions
-		    NBTTagList villages = compound.getTagList("Villages", 10);
-		    
-		    data = new PacketBuffer(Unpooled.buffer());
-		    data.writeInt(CarpetClientMessageHandler.LARGE_VILLAGE_MARKERS_START);
-		    data.writeVarInt(villages.tagCount());
-		    CarpetClientServer.sender(data, sender);
-		    
-		    for (int i = 0; i < villages.tagCount(); i += 256) {
-		        data = new PacketBuffer(Unpooled.buffer());
-		        data.writeInt(CarpetClientMessageHandler.LARGE_VILLAGE_MARKERS);
-		        data.writeByte(Math.min(villages.tagCount() - i - 1, 255));
-		        for (int j = i; j < villages.tagCount() && j < i + 256; j++) {
-		            data.writeCompoundTag(villages.getCompoundTagAt(j));
-		        }
-		        CarpetClientServer.sender(data, sender);
-		    }
-		}
-	}
+        data.writeCompoundTag(compound);
 
-	public static void sendNBTBoundingboxData(EntityPlayerMP sender, NBTTagCompound compound) {
-		PacketBuffer data = new PacketBuffer(Unpooled.buffer());
-		data.writeInt(CarpetClientMessageHandler.BOUNDINGBOX_MARKERS);
+        if (!CarpetClientServer.sendProtected(data, sender)) {
+            // Payload was too large, try large packets for newer CC versions
+            NBTTagList villages = compound.getTagList("Villages", 10);
 
-		data.writeCompoundTag(compound);
+            data = new PacketBuffer(Unpooled.buffer());
+            data.writeInt(CarpetClientMessageHandler.LARGE_VILLAGE_MARKERS_START);
+            data.writeVarInt(villages.tagCount());
+            CarpetClientServer.sender(data, sender);
 
-		if (!CarpetClientServer.sendProtected(data, sender)) {
-		    // Payload was too large, try large packets for newer CC versions
-		    NBTTagList boxes = compound.getTagList("Boxes", 9);
-		    compound.removeTag("Boxes");
-		    List<NBTTagCompound> allBoxes = new ArrayList<>();
-		    for (int i = 0; i < boxes.tagCount(); i++) {
-		        NBTTagList list = (NBTTagList) boxes.get(i);
-		        for (int j = 0; j < list.tagCount(); j++) {
-		            allBoxes.add(list.getCompoundTagAt(j));
-		        }
-		    }
-		    
-		    data = new PacketBuffer(Unpooled.buffer());
-		    data.writeInt(CarpetClientMessageHandler.LARGE_BOUNDINGBOX_MARKERS_START);
-		    data.writeCompoundTag(compound);
-		    data.writeVarInt(allBoxes.size());
-		    CarpetClientServer.sender(data, sender);
-		    
-		    for (int i = 0; i < allBoxes.size(); i += 256) {
-		        data = new PacketBuffer(Unpooled.buffer());
-		        data.writeInt(CarpetClientMessageHandler.LARGE_BOUNDINGBOX_MARKERS);
-		        data.writeByte(Math.min(allBoxes.size() - i - 1, 255));
-		        for (int j = i; j < allBoxes.size() && j < i + 256; j++)
-		            data.writeCompoundTag(allBoxes.get(j));
-		        CarpetClientServer.sender(data, sender);
-		    }
-		}
-	}
+            for (int i = 0; i < villages.tagCount(); i += 256) {
+                data = new PacketBuffer(Unpooled.buffer());
+                data.writeInt(CarpetClientMessageHandler.LARGE_VILLAGE_MARKERS);
+                data.writeByte(Math.min(villages.tagCount() - i - 1, 255));
+                for (int j = i; j < villages.tagCount() && j < i + 256; j++) {
+                    data.writeCompoundTag(villages.getCompoundTagAt(j));
+                }
+                CarpetClientServer.sender(data, sender);
+            }
+        }
+    }
 
-	public static void sendTickRateChanges() {
-		PacketBuffer data = new PacketBuffer(Unpooled.buffer());
-		data.writeInt(CarpetClientMessageHandler.TICKRATE_CHANGES);
-		data.writeFloat(TickSpeed.tickrate);
+    public static void sendNBTBoundingboxData(EntityPlayerMP sender, NBTTagCompound compound) {
+        PacketBuffer data = new PacketBuffer(Unpooled.buffer());
+        data.writeInt(CarpetClientMessageHandler.BOUNDINGBOX_MARKERS);
 
-		CarpetClientServer.sender(data);
-	}
-	
+        data.writeCompoundTag(compound);
+
+        if (!CarpetClientServer.sendProtected(data, sender)) {
+            // Payload was too large, try large packets for newer CC versions
+            NBTTagList boxes = compound.getTagList("Boxes", 9);
+            compound.removeTag("Boxes");
+            List<NBTTagCompound> allBoxes = new ArrayList<>();
+            for (int i = 0; i < boxes.tagCount(); i++) {
+                NBTTagList list = (NBTTagList) boxes.get(i);
+                for (int j = 0; j < list.tagCount(); j++) {
+                    allBoxes.add(list.getCompoundTagAt(j));
+                }
+            }
+
+            data = new PacketBuffer(Unpooled.buffer());
+            data.writeInt(CarpetClientMessageHandler.LARGE_BOUNDINGBOX_MARKERS_START);
+            data.writeCompoundTag(compound);
+            data.writeVarInt(allBoxes.size());
+            CarpetClientServer.sender(data, sender);
+
+            for (int i = 0; i < allBoxes.size(); i += 256) {
+                data = new PacketBuffer(Unpooled.buffer());
+                data.writeInt(CarpetClientMessageHandler.LARGE_BOUNDINGBOX_MARKERS);
+                data.writeByte(Math.min(allBoxes.size() - i - 1, 255));
+                for (int j = i; j < allBoxes.size() && j < i + 256; j++)
+                    data.writeCompoundTag(allBoxes.get(j));
+                CarpetClientServer.sender(data, sender);
+            }
+        }
+    }
+
+    public static void sendTickRateChanges() {
+        PacketBuffer data = new PacketBuffer(Unpooled.buffer());
+        data.writeInt(CarpetClientMessageHandler.TICKRATE_CHANGES);
+        data.writeFloat(TickSpeed.tickrate);
+
+        CarpetClientServer.sender(data);
+    }
+
     public static void sendNBTChunkData(EntityPlayerMP sender, int dataType, NBTTagCompound compound) {
         PacketBuffer data = new PacketBuffer(Unpooled.buffer());
         data.writeInt(CarpetClientMessageHandler.CHUNK_LOGGER);
         data.writeInt(dataType);
-        data.writeCompoundTag(compound);
-
+        try {
+            data.writeCompoundTag(compound);
+        }catch(Exception e){ }
         CarpetClientServer.sender(data, sender);
     }
 }
