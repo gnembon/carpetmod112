12e81e115f20b1efe088de839101afd111c8062f
Simon Wanner <skyrising@pvpctutorials.de>
2019-02-24 17:02:45 +0100

    Refactor plugin channel management

--- carpetmodSrc/carpet/CarpetServer.java
+++ carpetmodSrc/carpet/CarpetServer.java
@@ -1,22 +1,18 @@
 package carpet;
 
 import carpet.helpers.StackTraceDeobfuscator;
+import carpet.network.PluginChannelManager;
+import carpet.network.ToggleableChannelHandler;
 import carpet.utils.HUDController;
-import carpet.utils.PluginChannelTracker;
 import carpet.utils.TickingArea;
 import carpet.utils.Waypoint;
 import carpet.worldedit.WorldEditBridge;
 
-import com.google.common.base.Charsets;
-import com.google.common.collect.Lists;
-
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.List;
 import java.util.Random;
 
 import narcolepticfrog.rsmm.events.PlayerConnectionEventDispatcher;
-import narcolepticfrog.rsmm.events.ServerPacketEventDispatcher;
 import narcolepticfrog.rsmm.events.TickStartEventDispatcher;
 import narcolepticfrog.rsmm.server.RSMMServer;
 
@@ -25,22 +21,30 @@ import carpet.carpetclient.CarpetClientServer;
 import carpet.helpers.TickSpeed;
 import net.minecraft.entity.player.EntityPlayerMP;
 import carpet.logging.LoggerRegistry;
-import net.minecraft.network.PacketBuffer;
-import net.minecraft.network.play.client.CPacketCustomPayload;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.WorldServer;
 
 public class CarpetServer // static for now - easier to handle all around the code, its one anyways
 {
     public static final Random rand = new Random((int)((2>>16)*Math.random()));
+    public static MinecraftServer minecraft_server;
+
+    public static PluginChannelManager pluginChannels;
     private static CarpetClientServer CCServer;
     public static RSMMServer rsmmServer;
-    public static MinecraftServer minecraft_server;
+    public static ToggleableChannelHandler rsmmChannel;
+    public static ToggleableChannelHandler wecuiChannel;
+
     public static void init(MinecraftServer server) //aka constructor of this static singleton class
     {
+        minecraft_server = server;
+        pluginChannels = new PluginChannelManager(server);
         CCServer = new CarpetClientServer(server);
+        pluginChannels.register(CCServer);
+
         rsmmServer = new RSMMServer(server);
-        CarpetServer.minecraft_server = server;
+        rsmmChannel = new ToggleableChannelHandler(pluginChannels, rsmmServer.createChannelHandler(), false);
+        wecuiChannel = new ToggleableChannelHandler(pluginChannels, WorldEditBridge.createChannelHandler(), false);
     }
     public static void onServerLoaded(MinecraftServer server)
     {
@@ -90,57 +94,14 @@ public class CarpetServer // static for now - easier to handle all around the co
     }
     public static void playerConnected(EntityPlayerMP player)
     {
-        if (CarpetSettings.redstoneMultimeter)
-            PlayerConnectionEventDispatcher.dispatchPlayerConnectEvent(player);
+        pluginChannels.onPlayerConnected(player);
         LoggerRegistry.playerConnected(player);
-        CCServer.onPlayerConnect(player);
     }
 
     public static void playerDisconnected(EntityPlayerMP player)
     {
-        if (CarpetSettings.redstoneMultimeter) // optionally send anyways (Frog's decision) decision
-        {
-            PlayerConnectionEventDispatcher.dispatchPlayerDisconnectEvent(player);
-            PluginChannelTracker.unregisterAll(player);
-        }
+        pluginChannels.onPlayerDisconnected(player);
         LoggerRegistry.playerDisconnected(player);
-        CCServer.onPlayerDisconnect(player);
-    }
-
-    //network stuffs
-    public static void customPacket(EntityPlayerMP playerEntity, String packet_id, CPacketCustomPayload packetIn)
-    {
-        if ("REGISTER".equals(packet_id)) {
-            List<String> channels = getChannels(packetIn.getBufferData());
-            for (String channel : channels) {
-                PluginChannelTracker.register(playerEntity, channel);
-            }
-            if (CarpetSettings.redstoneMultimeter)
-                ServerPacketEventDispatcher.dispatchChannelRegister(playerEntity, channels);
-            CCServer.onChannelRegister(playerEntity, channels);
-        } else if ("UNREGISTER".equals(packet_id)) {
-            List<String> channels = getChannels(packetIn.getBufferData());
-            for (String channel : channels) {
-                PluginChannelTracker.unregister(playerEntity, channel);
-            }
-            if (CarpetSettings.redstoneMultimeter)
-                ServerPacketEventDispatcher.dispatchChannelUnregister(playerEntity, channels);
-            CCServer.onChannelUnregister(playerEntity, channels);
-        } else {
-            if (CarpetSettings.redstoneMultimeter)
-                ServerPacketEventDispatcher.dispatchCustomPayload(playerEntity, packet_id, packetIn.getBufferData());
-            CCServer.onCustomPayload(playerEntity, packet_id, packetIn.getBufferData());
-            WorldEditBridge.onCustomPayload(packetIn, playerEntity);
-        }
-    }
-
-    private static List<String> getChannels(PacketBuffer buff) {
-        buff.resetReaderIndex();
-        byte[] bytes = new byte[buff.readableBytes()];
-        buff.readBytes(bytes);
-        String channelString = new String(bytes, Charsets.UTF_8);
-        return Lists.newArrayList(channelString.split("\u0000"));
-        //return channels;
     }
     
     public static Random setRandomSeed(int p_72843_1_, int p_72843_2_, int p_72843_3_)
