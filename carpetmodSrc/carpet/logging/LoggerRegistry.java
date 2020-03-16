bdad4a3519a1511e0e0dae51c3d911729b1ecbcd
Unknown <sahandhesar@gmail.com>
2020-03-16 20:19:18 +0100

    Added potion duration debugger

--- carpetmodSrc/carpet/logging/LoggerRegistry.java
+++ carpetmodSrc/carpet/logging/LoggerRegistry.java
@@ -47,6 +47,7 @@ public class LoggerRegistry
     public static boolean __items;
     public static boolean __rng;
     public static boolean __recipes;
+    public static boolean __potion;
 
     public static void initLoggers(MinecraftServer server)
     {
@@ -68,6 +69,7 @@ public class LoggerRegistry
         registerLogger("mobcaps", new Logger(server, "mobcaps", "dynamic",new String[]{"dynamic", "overworld", "nether","end"}, LogHandler.HUD));
 
         registerDebugger("recipes", new Logger(server, "recipes", null, null, LogHandler.CHAT));
+        registerDebugger("potion", new Logger(server, "potion", null, null, LogHandler.CHAT));
     }
 
     private static File getSaveFile(MinecraftServer server) { return server.getActiveAnvilConverter().getFile(server.getFolderName(), "loggerData.json"); }
