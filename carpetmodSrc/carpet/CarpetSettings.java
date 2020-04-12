a0213a9aefd6ccec91b9fc379ead8df20ed140f7
Unknown <sahandhesar@gmail.com>
2020-04-13 00:46:27 +0200

    Version bump to v20_04_13

--- carpetmodSrc/carpet/CarpetSettings.java
+++ carpetmodSrc/carpet/CarpetSettings.java
@@ -44,7 +44,7 @@ public class CarpetSettings
     public static boolean locked = false;
 
     // TODO: replace these constants at build time
-    public static final String carpetVersion = "v20_04_11";
+    public static final String carpetVersion = "v20_04_13";
     public static final String minecraftVersion = "1.12.2";
     public static final String mcpMappings = "39-1.12";
 
