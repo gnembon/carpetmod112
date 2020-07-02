dc20d2e1e17856f68c37f24a4967eb747d1c65f4
gsquaredxc <ggarrison5@gmail.com>
2020-07-02 16:42:21 -0500

    Optimize and clean up InstantComparators

--- carpetmodSrc/carpet/logging/logHelpers/InstantComparators.java
+++ carpetmodSrc/carpet/logging/logHelpers/InstantComparators.java
@@ -4,11 +4,10 @@ import carpet.logging.LoggerRegistry;
 import carpet.utils.Messenger;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.world.World;
 
 public class InstantComparators {
 
-    public static void onNoTileEntity(World world, BlockPos pos) {
+    public static void onNoTileEntity(BlockPos pos) {
         if (LoggerRegistry.__instantComparators) {
             LoggerRegistry.getLogger("instantComparators").log(option -> new ITextComponent[] {
                         Messenger.m(null, "y Comparator has no tile entity ", Messenger.tp("y", pos))
@@ -19,10 +18,10 @@ public class InstantComparators {
         }
     }
 
-    public static void onInstantComparator(World world, BlockPos pos, boolean buggy) {
+    public static void onInstantComparator(BlockPos pos, boolean buggy) {
         if (LoggerRegistry.__instantComparators) {
             LoggerRegistry.getLogger("instantComparators").log(option -> {
-                if ((!buggy && !"buggy".equals(option)) || (buggy && !"tileTick".equals(option))) {
+                if (buggy ? !"tileTick".equals(option) : !"buggy".equals(option)) {
                     return new ITextComponent[] {
                             Messenger.m(null, "l " + (buggy ? "Buggy" : "Tile tick") + " instant comparator detected ", Messenger.tp("y", pos))
                     };
