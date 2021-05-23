b9d68bc5df7e203a10091eb4a8562618095ef7a8
Kyle Weber <38956382+kweber1@users.noreply.github.com>
2021-05-23 17:32:24 -0400

    Added more commands and better implantation

--- carpetmodSrc/carpet/commands/CommandLazyChunkBehavior.java
+++ carpetmodSrc/carpet/commands/CommandLazyChunkBehavior.java
@@ -7,7 +7,9 @@ import net.minecraft.command.WrongUsageException;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 import net.minecraft.world.chunk.Chunk;
 
 import javax.annotation.Nullable;
@@ -25,13 +27,65 @@ public class CommandLazyChunkBehavior extends CommandCarpetBase{
 
     public String getUsage(ICommandSender sender)
     {
-        return "Usage: lazychunkbehavior <X> <Z>";
+        return "lazychunkbehavior <add, removeAll, remove, list> <X> <Z>";
     }
 
     public String getName()
     {
         return "lazychunkbehavior";
     }
+
+    private void list(ICommandSender sender, String[] args) throws CommandException{
+        if (args.length != 1)
+        {
+            throw new WrongUsageException(getUsage(sender), new Object[0]);
+        }
+        LazyChunkBehaviorHelper.listLazyChunks(sender);
+    }
+    private void removeAll(ICommandSender sender, String[] args) throws CommandException{
+        if (args.length != 1)
+        {
+            throw new WrongUsageException(getUsage(sender), new Object[0]);
+        }
+        LazyChunkBehaviorHelper.removeAll();
+        TextComponentString text = new TextComponentString("All chunks have been removed." );
+        text.getStyle().setColor(TextFormatting.RED);
+        sender.sendMessage(text);
+    }
+
+    private void add(ICommandSender sender, String[] args) throws CommandException{
+
+        if (args.length != 3)
+        {
+            throw new WrongUsageException(getUsage(sender), new Object[0]);
+        }
+        int chunkX = parseInt(args[1]);
+        int chunkZ = parseInt(args[2]);
+        World world = sender.getEntityWorld();
+        Chunk chunk = world.getChunk(chunkX, chunkZ);
+        LazyChunkBehaviorHelper.addLazyChunk(chunk);
+        TextComponentString text = new TextComponentString("Chunk " + chunkX + ", " + chunkZ + " in world " + chunk.getWorld().provider.getDimensionType() + " has been added." );
+        text.getStyle().setColor(TextFormatting.RED);
+        sender.sendMessage(text);
+    }
+
+    private void remove(ICommandSender sender, String[] args) throws CommandException{
+
+        if (args.length != 3)
+        {
+            throw new WrongUsageException(getUsage(sender), new Object[0]);
+        }
+        int chunkX = parseInt(args[1]);
+        int chunkZ = parseInt(args[2]);
+        World world = sender.getEntityWorld();
+        Chunk chunk = world.getChunk(chunkX, chunkZ);
+        LazyChunkBehaviorHelper.removeLazyChunk(chunk);
+        TextComponentString text = new TextComponentString("Chunk " + chunkX + ", " + chunkZ + " in world " + chunk.getWorld().provider.getDimensionType() + " has been removed." );
+        text.getStyle().setColor(TextFormatting.RED);
+        sender.sendMessage(text);
+    }
+
+
 
     /**
      * Callback for when the command is executed
@@ -40,23 +94,34 @@ public class CommandLazyChunkBehavior extends CommandCarpetBase{
     {
         if (!command_enabled("CommandLazyChunkBehavior", sender)) return;
 
-        if (args.length != 2)
-        {
+        if(args.length<1 || args.length>3){
             throw new WrongUsageException(getUsage(sender), new Object[0]);
         }
-        int chunkX = parseInt(args[0]);
-        int chunkZ = parseInt(args[1]);
-        World world = sender.getEntityWorld();
-        Chunk chunk = world.getChunk(chunkX, chunkZ);
-        sender.sendMessage(new TextComponentString(  "\u00A74\u00A7lChunk " + chunkX + ", " + chunkZ + " " + LazyChunkBehaviorHelper.CommandFeedBack(chunk)));
+        switch (args[0]){
+            case "list":
+                list(sender,args);
+                return;
+            case "add":
+                add(sender,args);
+                return;
+            case "remove":
+                remove(sender,args);
+                return;
+            case "removeAll":
+                removeAll(sender,args);
+        }
     }
     public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
         int chunkX = sender.getPosition().getX() >> 4;
         int chunkZ = sender.getPosition().getZ() >> 4;
 
         if (args.length == 1) {
+            return getListOfStringsMatchingLastWord(args, "list", "add", "remove","removeAll");
+        }
+        else if(args.length == 2 && !args[0].equals("list") && !args[0].equals("removeAll")){
             return getListOfStringsMatchingLastWord(args, Integer.toString(chunkX));
-        } else if (args.length == 2) {
+        }
+        else if (args.length == 3 && !args[0].equals("list") && !args[0].equals("removeAll")) {
             return getListOfStringsMatchingLastWord(args, Integer.toString(chunkZ));
         } else {
             return Collections.emptyList();
--- carpetmodSrc/carpet/helpers/LazyChunkBehaviorHelper.java
+++ carpetmodSrc/carpet/helpers/LazyChunkBehaviorHelper.java
@@ -1,6 +1,12 @@
 package carpet.helpers;
 
+import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
@@ -12,14 +18,33 @@ import static carpet.CarpetSettings.commandLazyChunkBehavior;
 public class LazyChunkBehaviorHelper {
     private static final ArrayList<Chunk> lazyProcessingChunks = new ArrayList<Chunk>();
 
-    private static boolean addLazyChunk(Chunk chunk){
+    public static void listLazyChunks(ICommandSender sender){
+        if(!lazyProcessingChunks.isEmpty()) {
+            for (Chunk chunk : lazyProcessingChunks) {
+                TextComponentString text = new TextComponentString("Chunk " + chunk.x + ", " + chunk.z + " in world " + chunk.getWorld().provider.getDimensionType());
+                text.getStyle().setColor(TextFormatting.GREEN);
+                sender.sendMessage(text);
+            }
+
+        }
+        else{
+            TextComponentString text = new TextComponentString("No chunks to list." );
+            text.getStyle().setColor(TextFormatting.GREEN);
+            sender.sendMessage(text);
+        }
+    }
+    public static void removeAll(){
+        lazyProcessingChunks.clear();
+    }
+
+    public static boolean addLazyChunk(Chunk chunk){
         if(!containsLazyChunk(chunk)) {
             lazyProcessingChunks.add(chunk);
             return true;
         }
         return false;
     }
-    private static boolean removeLazyChunk(Chunk chunk){
+    public static boolean removeLazyChunk(Chunk chunk){
         if(containsLazyChunk(chunk)) {
             lazyProcessingChunks.remove(chunk);
             return true;
@@ -31,26 +56,18 @@ public class LazyChunkBehaviorHelper {
            return false;
        return lazyProcessingChunks.contains(chunk);
     }
-    public static String CommandFeedBack(Chunk chunk){
-        if (chunk==null){
-            return "Failed to add! Chunk is NULL.";
-        }
-        else if(addLazyChunk(chunk))
-            return "Added.";
-        else if(removeLazyChunk(chunk))
-            return "Removed.";
-        else
-            return "Not Added or Remove. The command is disabled.";
-    }
 
-    public static boolean shouldUpdateEntity(Entity entityIn) {
+    public static boolean shouldUpdate(Entity entityIn) {
         World world = entityIn.getEntityWorld();
-        Chunk chunk = world.getChunk(entityIn.getPosition().getX() >> 4, entityIn.getPosition().getZ() >> 4);
-        return !containsLazyChunk(chunk);
+        return shouldUpdate(world, entityIn.getPosition());
     }
 
-    public static boolean shouldUpdateFallingBlock(World worldIn, int x, int z) {
-        Chunk chunk = worldIn.getChunk(x >> 4, z >> 4);
+    public static boolean shouldUpdate(World worldIn, BlockPos pos) {
+        return shouldUpdate(worldIn, new ChunkPos(pos.getX()>> 4,pos.getZ()>> 4));
+    }
+
+    public static boolean shouldUpdate(World worldIn, ChunkPos pos) {
+        Chunk chunk = worldIn.getChunk(pos.x, pos.z);
         return !containsLazyChunk(chunk);
     }
 }
